a6e2a4da5bb11bdbea3ca8e9de41a3071c0ac3a1
Gaetan <gaetan@ryter.ninja>
2020-01-29 16:17:57 +0100

    Remove host validation in Extension.

--- DependencyInjection/MobileDetectExtension.php
+++ DependencyInjection/MobileDetectExtension.php
@@ -34,21 +34,6 @@ class MobileDetectExtension extends Extension
         $loader->load('listener.xml');
         $loader->load('twig.xml');
 
-        // valid mobile host
-        if ($config['redirect']['mobile']['is_enabled'] && !$this->validHost($config['redirect']['mobile']['host'])) {
-            $config['redirect']['mobile']['is_enabled'] = false;
-        }
-
-        // valid tablet host
-        if ($config['redirect']['tablet']['is_enabled'] && !$this->validHost($config['redirect']['tablet']['host'])) {
-            $config['redirect']['tablet']['is_enabled'] = false;
-        }
-
-        // valid full host
-        if ($config['redirect']['full']['is_enabled'] && !$this->validHost($config['redirect']['full']['host'])) {
-            $config['redirect']['full']['is_enabled'] = false;
-        }
-
         $container->setParameter('mobile_detect.redirect', $config['redirect']);
         $container->setParameter('mobile_detect.switch_device_view.save_referer_path', $config['switch_device_view']['save_referer_path']);
 
@@ -66,18 +51,5 @@ class MobileDetectExtension extends Extension
         $container->setParameter('mobile_detect.twig.extension.class', $config['twig_extension_class']);
 
         $container->setAlias('mobile_detect.mobile_detector', $config['service']['mobile_detector']);
-    }
-
-    /**
-     * Validate host
-     * @param string $url
-     *
-     * @return boolean
-     */
-    protected function validHost($url)
-    {
-        $pattern = "/^(?:(http|https):\/\/)([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i";
-
-        return (bool) preg_match($pattern, $url);
     }
 }
